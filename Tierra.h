80b06bf80fc08083dc6c66b86e59a030ad76f8aa
lizbetcs2015 <51393334+lizbetcs2015@users.noreply.github.com>
2019-06-23 11:46:35 -0500

    Update Tierra.h

--- Tierra.h
+++ Tierra.h
@@ -1,9 +1,38 @@
-//
-// Created by utec on 21/06/19.
-//
 
 #ifndef POO_TIERRA_H
 #define POO_TIERRA_H
+#ifndef AGREGACION_TIERRA_H
+#define AGREGACION_TIERRA_H
+#include <iostream>
+#include <vector>
+#include "Tipos.h"
+#include "Objeto.h"
+using namespace std;
+// Valores constantes
+const TipoEntero ALTURA = 21;
+const TipoEntero ANCHO  = 21;
+const TipoCaracter COLOR ='.';
+
+class Tierra {
+private:
+   vector<vector<char>> plano;
+   vector<Objeto*> objetos;
+public:
+   Tierra();
+   Tierra(TipoEntero altura, TipoEntero ancho);
+   virtual ~Tierra();
+   void adicionarObjeto(Objeto* objeto);
+   Objeto* removerObjeto(string& nombre);
+   void imprimirObjetos();
+   TipoEntero getAltura();
+   TipoEntero getAncho();
+   TipoEntero getCantidadObjectos();
+   void dibujarTierra();
+   void actualizarTierra();
+};
+
+#endif //AGREGACION_TIERRA_H
+
 
 
 class Tierra {
