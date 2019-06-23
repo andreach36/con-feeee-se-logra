2d5a86608bfe499c290e2b32cf9db77c560ffb20
lizbetcs2015 <51393334+lizbetcs2015@users.noreply.github.com>
2019-06-22 19:24:55 -0500

    Update Controlador.h

--- Controlador.h
+++ Controlador.h
@@ -4,6 +4,37 @@
 
 #ifndef POO_CONTROLADOR_H
 #define POO_CONTROLADOR_H
+#ifndef AGREGACION_MENU_H
+#define AGREGACION_MENU_H
+
+#include "Tierra.h"
+#include "Tipos.h"
+
+template <typename T>
+T input(string label) {
+ T value;
+ cout << label;
+ cin >> value;
+ cin.clear();
+ cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+ return value;
+}
+class Menu {
+   TipoEntero opcion;
+   Tierra tierra;
+   void imprimirMenu();
+   void seleccionarOpcion();
+   void agregarObjeto();
+   void removerObjeto();
+   void dibujarMapa();
+public:
+   Menu(): opcion{} {}
+   void ejecutar();
+};
+
+#endif //AGREGACION_MENU_H
+
+
 
 
 class Controlador {
