58020b2f599b293b6f9a6637af38d42db7e16b9e
walter_diaz <walter.diaz@utec.edu.pe>
2019-06-23 19:24:12 -0500

    2da

--- Objetos.cpp
+++ Objetos.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "Objetos.h"
+#ifndef AGREGACION_OBJETO_H
+#define AGREGACION_OBJETO_H
+#include <iostream>
+#include "Tipos.h"
+using namespace std;
+
+class Objeto {
+private:
+   string        nombre;
+   TipoCaracter  color;
+   TipoEntero    posX;
+   TipoEntero    posY;
+public:
+   Objeto();
+   Objeto(const TipoString& nombre, TipoCaracter color,
+                TipoEntero posX, TipoEntero posY);
+   virtual ~Objeto();
+   void setNombre(const TipoString& nombre);
+   string     getNombre();
+   TipoEntero getPosX();
+   TipoEntero getPosY();
+   char getColor();
+   void moverse(TipoEntero x, TipoEntero y);
+   string mostrarPosicion();
+};
+
+#endif //AGREGACION_OBJETO_H
+
