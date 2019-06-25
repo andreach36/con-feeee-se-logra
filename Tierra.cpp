d801363aa90fa63107ef3cc47e7a1a4a5fc9055f
lizbetcs2015 <51393334+lizbetcs2015@users.noreply.github.com>
2019-06-25 15:06:12 -0500

    Update Tierra.cpp

--- Tierra.cpp
+++ Tierra.cpp
@@ -1,47 +1,51 @@
 //
 // Created by utec on 21/06/19.
 //
-
 #include "Tierra.h"
 #include <string>
 #include <iomanip>
+#include <map>
 #include <algorithm>
+#include "Objeto.h"
+
+
+map<char, sf::Color> listaColores = {
+        {'R', sf::Color::Red },
+        {'G', sf::Color::Green},
+        {'B', sf::Color::Blue}};
+
 
 using namespace std;
-
 Tierra::Tierra() {
-    plano.resize(ALTURA);
-    for (auto& item: plano)
-        item.resize(ANCHO);
+    plano = nullptr;
 }
-
 Tierra::Tierra(TipoEntero altura, TipoEntero ancho) {
-    plano.resize(altura);
-    for (auto& item: plano)
-        item.resize(ancho);
+    plano = new sf::RenderWindow(sf::VideoMode(ancho, altura), "SFML works!");
 }
-
-Tierra::~Tierra() {}
-
+Tierra::~Tierra() {
+    delete plano;
+}
 void Tierra::adicionarObjeto(Objeto* objeto) {
     objetos.emplace_back(objeto);
 }
-
 Objeto* Tierra::removerObjeto(string& nombre) {
-    // Buscando objeto
+// Si vector esta vacio
     if (objetos.size() == 0)
-      return nullptr;
-
+        return nullptr;
+// Buscando objeto
     auto iter = find_if(begin(objetos), end(objetos),
                         [&nombre](Objeto* obj){ return obj->getNombre() == nombre; });
-    if (iter == end(objetos))
-      return nullptr;
-    // Eliminando objeto
-    objetos.erase(iter);
-    //-- si encuentra al objeto lo separa del vector, pero no mata al objeto, esto se hara en el menu.
-    return *iter;
+    if (iter != end(objetos)) {
+// Eliminando la referencia al puntero objeto dentro del vector objetos
+        objetos.erase(iter);
+//-- si encuentra al objeto lo separa del vector,
+//-- (no libera de memoria el objeto encontrado), esto se hará en el menú,
+//-- donde fue asignado, debido a que el objeto no es parte sino el objeto es un visitante.
+        return *iter;
+    }
+// Si vector esta vacio
+    return nullptr;
 }
-
 void Tierra::imprimirObjetos() {
     int i = 0;
     for (auto& item: objetos) {
@@ -52,37 +56,41 @@ void Tierra::imprimirObjetos() {
         i++;
     }
 }
-
 void Tierra::actualizarTierra() {
-    for (auto &row: plano)
-        for (auto &cell: row)
-            cell = COLOR;
-
-    for (auto& item: objetos)
-        plano[item->getPosX()][item->getPosY()]
-                = item->getColor();
+//    for (auto &row: plano)
+//        for (auto &cell: row)
+//            cell = COLOR;
+//    for (auto& item: objetos)
+//        plano[item->getPosX()][item->getPosY()]
+//                = item->getColor();
 }
-
 void Tierra::dibujarTierra() {
-    cout << '\n';
-    cout << setw(3) << ' ';
-    for (auto j = 0; j < getAncho(); ++j)
-        cout << setw(3) << j;
-    cout << '\n';
 
-    for (auto i = 0; i < getAltura(); ++i) {
-        cout << setw(3) << i;
-        for (auto j = 0; j < getAncho(); ++j) {
-            cout << setw(3) << plano[i][j];
+    while (plano->isOpen())
+    {
+        sf::Event event;
+        while (plano->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                plano->close();
         }
-        cout << '\n';
+        plano->clear();
+        for (auto obj: objetos) {
+            sf::CircleShape shape(10);
+            shape.setPosition(obj->getPosX(), obj->getPosY());
+            shape.setFillColor(listaColores[obj->getColor()]);
+            plano->draw(shape);
+        }
+        plano->display();
     }
 }
-
 TipoEntero Tierra::getAltura() {
-    return plano.size();
+    return plano->getSize().y;
+}
+TipoEntero Tierra::getAncho(){
+    return plano->getSize().x;
 }
 
-TipoEntero Tierra::getAncho(){
-    return plano[0].size();
+TipoEntero Tierra::getCantidadObjectos() {
+    return objetos.size();
 }
